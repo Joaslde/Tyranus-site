ba8782f90b8c4d65818ccb6782f8c30c9278cdd6
Joas's-VsCode <ldejoas@gmail.com>
2026-04-16 08:51:52 +0100

    test

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet';
 import { CalendarDays, ArrowRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
+
 const Events = () => {
   const articles = [
     {
